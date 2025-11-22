dadc6f4a5d680f3c231dcac9a8abf5696e57381f
sergiovegaf <sergio.vegaf@usil.pe>
2025-11-21 21:05:10 -0500

    Delete src directory

--- src/main/java/Validador.java
+++
@@ -1,9 +0,0 @@
-public class Validador {
-
-    // 4.2 Implementación mínima válida (GREEN)
-    public static boolean soloLetras(String t) {
-        return t.equals("Hola");
-    }
-
-    // 4.3 Después se reemplaza por regex (REFRACTOR)
-}
