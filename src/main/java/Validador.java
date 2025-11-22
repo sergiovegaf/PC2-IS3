68aadcc7e95df1940f35b6ddd81f05e9a0e803df
sergiovegaf <sergio.vegaf@usil.pe>
2025-11-21 20:54:38 -0500

    Delete src directory

--- src/main/java/Validador.java
+++
@@ -1,8 +0,0 @@
-public class Validador {
-    
-    public static boolean soloLetras(String t) {
-        // 4.3 REFACTOR: Se mejoró usando "Regex" para validar el patrón en una sola línea sin bucles.
-        return t != null && t.matches("^[a-zA-Z]+$");
-    }
-    
-}
